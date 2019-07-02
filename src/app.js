0d016ccd2c3dba1d7a5ec46cb10288e974b0f91a
juanesquintero <juaneschrome@gmali.com>
2019-07-02 00:38:27 -0500

    URI

--- src/app.js
+++ src/app.js
@@ -53,6 +53,7 @@ app.use(bodyParser.urlencoded({extended: false}))
 
 app.use(require('./routes/index'))
 
+const urlDB = process.env.URI
 mongoose.connect(process.env.URI, {useNewUrlParser: true},(err, result)=>{
     if(err) return console.log(err)
     return console.log('Conectado a Mongo')
