79ee8e9b5993127c9fb4861b7dba62213f6c14bf
juanesquintero <juaneschrome@gmali.com>
2019-07-02 00:58:35 -0500

    URI :C

--- src/config/config.js
+++ src/config/config.js
@@ -1,3 +1,2 @@
 process.env.PORT = process.env.PORT || 3000
-// process.env.URI = "mongodb+srv://admin:admin123@cursos-extension-tdea-naeln.mongodb.net/educacion_continua?retryWrites=true&w=majority"
 process.env.URI = 'mongodb://localhost:27017/educacionContinua' 
